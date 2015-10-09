fdf19fccd65612b07121cdf6d7b398f25a638319
Manuel Herrera <manuel.he@gmail.com>
2015-10-09 04:31:30 -0500

    Event emitter for selected item

--- src/app/components/item_row/item_row.js
+++ src/app/components/item_row/item_row.js
@@ -10,12 +10,30 @@
         templateUrl: 'app/components/item_row/item_row.html',
         scope: {
           item: '=',
-          isSelected: '&'
+          selectedItem: '='
         },
         controller: function ($scope, statusCodes, itemTypes) {
+
           $scope.item.start_time = moment($scope.item.start_time);
           $scope.status = statusCodes[$scope.item.status] || statusCodes[1];
           $scope.typeDict = itemTypes[$scope.item.type];
+
+          $scope.isSelected = function () {
+            return $scope.selectedItem == $scope.item;
+          };
+
+          $scope.selectItem = function () {
+            if ($scope.item.status === 1) {
+              return;
+            }
+            var item = $scope.item;
+            if ($scope.isSelected()) {
+              item = {};
+            }
+
+            $scope.$emit('item:selected', item);
+          };
+
         }
       };
 
